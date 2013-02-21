45c315ed5070950f3efeb2f1279847a6dbb9327a
Vladimir Kikhtenko <kva911@gmail.com>
2013-02-21 12:37:07 +0700

    commented single file table

--- hvault/hvault--0.1.sql
+++ hvault/hvault--0.1.sql
@@ -14,31 +14,31 @@ CREATE FOREIGN DATA WRAPPER hvault_fdw
 
 CREATE SERVER hvault_service FOREIGN DATA WRAPPER hvault_fdw;
 
-CREATE FOREIGN TABLE test_single (
-    file_id   int4        OPTIONS (type 'file_index'),
-    line_id   int4        OPTIONS (type 'line_index'),
-    sample_id int4        OPTIONS (type 'sample_index'),
-    pnt       geometry    OPTIONS (type 'point'),
-    footpt    geometry    OPTIONS (type 'footprint'),
-    tstamp    timestamp   OPTIONS (type 'time'),
-    band1     float8      OPTIONS (sds  '1km Surface Reflectance Band 1'),
-    band2     float8      OPTIONS (sds  '1km Surface Reflectance Band 2'),
-    band3     float8      OPTIONS (sds  '1km Surface Reflectance Band 3'),
-    band4     float8      OPTIONS (sds  '1km Surface Reflectance Band 4'),
-    band5     float8      OPTIONS (sds  '1km Surface Reflectance Band 5'),
-    band6     float8      OPTIONS (sds  '1km Surface Reflectance Band 6'),
-    band7     float8      OPTIONS (sds  '1km Surface Reflectance Band 7'),
-    band8     float8      OPTIONS (sds  '1km Surface Reflectance Band 8'),
-    band9     float8      OPTIONS (sds  '1km Surface Reflectance Band 9'),
-    band10    float8      OPTIONS (sds  '1km Surface Reflectance Band 10'),
-    band11    float8      OPTIONS (sds  '1km Surface Reflectance Band 11'),
-    band12    float8      OPTIONS (sds  '1km Surface Reflectance Band 12'),
-    band13    float8      OPTIONS (sds  '1km Surface Reflectance Band 13'),
-    band14    float8      OPTIONS (sds  '1km Surface Reflectance Band 14'),
-    band15    float8      OPTIONS (sds  '1km Surface Reflectance Band 15'),
-    band16    float8      OPTIONS (sds  '1km Surface Reflectance Band 16')
-) SERVER hvault_service
-  OPTIONS (filename '/home/kikht/Downloads/MOD09.A2013026.0635.005.2013027075743.hdf');
+-- CREATE FOREIGN TABLE test_single (
+--     file_id   int4        OPTIONS (type 'file_index'),
+--     line_id   int4        OPTIONS (type 'line_index'),
+--     sample_id int4        OPTIONS (type 'sample_index'),
+--     pnt       geometry    OPTIONS (type 'point'),
+--     footpt    geometry    OPTIONS (type 'footprint'),
+--     tstamp    timestamp   OPTIONS (type 'time'),
+--     band1     float8      OPTIONS (sds  '1km Surface Reflectance Band 1'),
+--     band2     float8      OPTIONS (sds  '1km Surface Reflectance Band 2'),
+--     band3     float8      OPTIONS (sds  '1km Surface Reflectance Band 3'),
+--     band4     float8      OPTIONS (sds  '1km Surface Reflectance Band 4'),
+--     band5     float8      OPTIONS (sds  '1km Surface Reflectance Band 5'),
+--     band6     float8      OPTIONS (sds  '1km Surface Reflectance Band 6'),
+--     band7     float8      OPTIONS (sds  '1km Surface Reflectance Band 7'),
+--     band8     float8      OPTIONS (sds  '1km Surface Reflectance Band 8'),
+--     band9     float8      OPTIONS (sds  '1km Surface Reflectance Band 9'),
+--     band10    float8      OPTIONS (sds  '1km Surface Reflectance Band 10'),
+--     band11    float8      OPTIONS (sds  '1km Surface Reflectance Band 11'),
+--     band12    float8      OPTIONS (sds  '1km Surface Reflectance Band 12'),
+--     band13    float8      OPTIONS (sds  '1km Surface Reflectance Band 13'),
+--     band14    float8      OPTIONS (sds  '1km Surface Reflectance Band 14'),
+--     band15    float8      OPTIONS (sds  '1km Surface Reflectance Band 15'),
+--     band16    float8      OPTIONS (sds  '1km Surface Reflectance Band 16')
+-- ) SERVER hvault_service
+--   OPTIONS (filename '/home/kikht/Downloads/MOD09.A2013026.0635.005.2013027075743.hdf');
 
 CREATE TABLE hdf_catalog (
     file_id   serial    PRIMARY KEY,
