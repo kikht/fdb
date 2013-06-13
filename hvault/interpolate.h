e40c2efaa8a468e914e65b34c5fbf28632c2412e
Vladimir Kikhtenko <kva911@gmail.com>
2013-06-13 19:23:35 +0700

    new interpolation routines

--- hvault/interpolate.h
+++ hvault/interpolate.h
@@ -6,4 +6,20 @@
 void interpolate_line(size_t m, float const *p, float const *n, float *r);
 void extrapolate_line(size_t m, float const *p, float const *n, float *r);
 
+void hvaultInterpolateFootprintKernel (size_t size, float * kernel);
+void hvaultInterpolatePointKernel (size_t size, float *kernel);
+void hvaultInterpolateFootprint (float const * src,
+                                 float * dst,
+                                 size_t src_h, size_t src_w,
+                                 float const * kernel, size_t k);
+void hvaultInterpolateFootprint1x (float const * src, 
+                                   float * dst,
+                                   size_t src_h, size_t src_w);
+void hvaultInterpolateFootprint2x (float const * src, 
+                                   float * dst,
+                                   size_t src_h, size_t src_w);
+void hvaultInterpolateFootprint4x (float const * src, 
+                                   float * dst,
+                                   size_t src_h, size_t src_w);
+
 #endif
