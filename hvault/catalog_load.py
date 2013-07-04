ed511d314cf0e37bdf7fd31a3124d5cb0d36ae39
Vladimir Kikhtenko <kva911@gmail.com>
2013-07-04 16:45:57 +0700

    add nextlines to script

--- hvault/catalog_load.py
+++ hvault/catalog_load.py
@@ -24,23 +24,23 @@ prod_re["modhkmds"] = re.compile("^M[OY]DHKMDS.*hdf$")
 prod_re["mod1kmds"] = re.compile("^M[OY]D1KMDS.*hdf$")
 
 # TODO: insert connection parameters here
-connection = psycopg2.connect()
+connection = psycopg2.connect();
 cursor = connection.cursor()
-query = "create table " + table_name + 
+query = "create table " + table_name + \
         "( id        serial    primary key, \
            starttime timestamp not null, \
            stoptime  timestamp not null, \
-           footprint geometry  not null, " + 
-        " text, ".join(prod_re.keys()) + " );"
+           footprint geometry  not null, " + \
+        " text, ".join(prod_re.keys()) + " text );"
 cursor.execute(query)
 query = "create index on " + table_name + " using gist( footprint )";
 cursor.execute(query);
 
-query = "prepare catinsert( timestamp, timestamp, geometry" + 
-        ", text" * len(prod_re) + 
-        ") as insert into " + table_name + 
-        " (starttime, stoptime, footprint, " + 
-        ", ".join(prod_re.keys()) + ") values (" + 
+query = "prepare catinsert( timestamp, timestamp, geometry" + \
+        ", text" * len(prod_re) + \
+        ") as insert into " + table_name + \
+        " (starttime, stoptime, footprint, " + \
+        ", ".join(prod_re.keys()) + ") values (" + \
         ", ".join([ "${0}".format(i) for i in range(1, len(prod_re) + 4) ]) + ");"
 cursor.execute(query)
 
@@ -52,11 +52,14 @@ for path in glob.iglob("/mnt/ifs-gis/ftp/*/modis/archive/*/*/?????/????"):
     if len(mod03) != 1:
         print "Problem with MOD03 in ", path
         continue
+	if len(filter(prod_re['mod021km'].match, dirList)) != 1:
+		print "Problem with MOD021KM in ", path
+		continue
 
     gd = gdal.Open(path + "/" + mod03[0])
-    starttime = gd.GetMetadataItem("RANGEBEGINNINGDATE") + " " + 
+    starttime = gd.GetMetadataItem("RANGEBEGINNINGDATE") + " " + \
                 gd.GetMetadataItem("RANGEBEGINNINGTIME")
-    stoptime  = gd.GetMetadataItem("RANGEENDINGDATE") + " " + 
+    stoptime  = gd.GetMetadataItem("RANGEENDINGDATE") + " " + \
                 gd.GetMetadataItem("RANGEENDINGTIME")
 
     coord = dict()
