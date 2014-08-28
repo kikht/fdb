5a53ceee226e59ab54071b79dff3dec4f8e2623d
Vladimir Kikhtenko <kva911@gmail.com>
2014-08-28 18:38:25 +0700

    fix table count bugs

--- hvault/table_group.c
+++ hvault/table_group.c
@@ -41,13 +41,12 @@ intArrayIdx(ArrayType * array, int const * coord, bool noerror)
 {
     int * lbs;
     int * dims;
-    int i, res, prevsize;
+    int i, res;
 
     lbs = ARR_LBOUND(array);
     dims = ARR_DIMS(array);
 
     res = 0;
-    prevsize = 0;
     for (i = 0; i < array->ndim; i++)
     {
         int c = coord[i] - lbs[i];
@@ -59,8 +58,7 @@ intArrayIdx(ArrayType * array, int const * coord, bool noerror)
             else
                 elog(ERROR, "array out of bounds");
         }
-        res = prevsize * res + c;
-        prevsize = dims[i];
+        res = res * dims[i] + c;
     }
 
     return res;
@@ -73,9 +71,8 @@ hvault_table_count_step(PG_FUNCTION_ARGS)
     MemoryContext aggmemctx, oldmemctx;
     ArrayType * ctx;
     ArrayType * idx_array;
-    ArrayType * bounds_array;
     int i, pos_idx;
-    int * ctx_data;
+    int32_t * ctx_data;
     
     if (!AggCheckCallContext(fcinfo, &aggmemctx))
         elog(ERROR, "hvault_table_group_step called in non-aggregate context");
@@ -85,9 +82,10 @@ hvault_table_count_step(PG_FUNCTION_ARGS)
     if (ctx == NULL)
     {
         int ndim;
-        int * bounds_data;
+        int32_t * bounds_data;
         int dims[MAXDIM];
         int lbs[MAXDIM]; 
+        ArrayType * bounds_array;
 
         oldmemctx = MemoryContextSwitchTo(aggmemctx);
 
@@ -100,23 +98,30 @@ hvault_table_count_step(PG_FUNCTION_ARGS)
         bounds_array = PG_GETARG_ARRAYTYPE_P(2);
         Assert(bounds_array != NULL);
         Assert(bounds_array->elemtype == INT4OID);
-        Assert(!ARR_HASNULL(bounds_array)); /*FIXME: is it correct assertion?*/
 
         if (bounds_array->ndim != 2 || ARR_DIMS(bounds_array)[1] != 2)
             elog(ERROR, "bounds array size is invalid");
+
+        if (ARR_HASNULL(bounds_array))
+        {
+            int size = ARR_DIMS(bounds_array)[0] * ARR_DIMS(bounds_array)[1]; 
+            for (i = 0; i < (size + 7) / 8; i++)
+                if (ARR_NULLBITMAP(bounds_array)[i] != 0)
+                    elog(ERROR, "bounds array must not contain NULLs");
+        }
 
         ndim = ARR_DIMS(bounds_array)[0]; 
         if (ndim > MAXDIM)
             elog(ERROR, "too many dimensions, max supported is %d", MAXDIM);
 
-        bounds_data = (int *) ARR_DATA_PTR(bounds_array);
+        bounds_data = (int32_t *) ARR_DATA_PTR(bounds_array);
         for (i = 0; i < ndim; i++) 
         {
             int ubs;
 
             lbs[i] = bounds_data[2*i];
             ubs = bounds_data[2*i+1];
-            dims[i] = ubs - lbs[i] + 1;
+            dims[i] = ubs - lbs[i];
         }
 
         ctx = intArrayInit(ndim, dims, lbs);
@@ -132,7 +137,6 @@ hvault_table_count_step(PG_FUNCTION_ARGS)
     idx_array = PG_GETARG_ARRAYTYPE_P(1);
     Assert(idx_array != NULL);
     Assert(idx_array->elemtype == INT4OID);
-    Assert(!ARR_HASNULL(idx_array)); /* FIXME: is it correct assertion? */
 
     if (idx_array->ndim != 1)
         elog(ERROR, "group index array must have single dimension");
@@ -140,10 +144,29 @@ hvault_table_count_step(PG_FUNCTION_ARGS)
     if (ARR_DIMS(idx_array)[0] != ctx->ndim)
         elog(ERROR, "group index array length is inconsistent");
 
+    if (ARR_HASNULL(idx_array))
+    {
+        int size = ARR_DIMS(idx_array)[0];
+        for (i = 0; i < (size + 7) / 8; i++)
+            /* Skip elements with nulls */
+            if (ARR_NULLBITMAP(idx_array)[i] != 0)
+            {
+                elog(WARNING, "index array contains NULL, skipping");
+                PG_RETURN_ARRAYTYPE_P(ctx); 
+            }
+    }
+    
+
     pos_idx = intArrayIdx(ctx, (int *) ARR_DATA_PTR(idx_array), true);
     if (pos_idx != -1)
     {
-        ctx_data = (int *) ARR_DATA_PTR(ctx);
+        Assert(pos_idx >= 0);
+        if (ARR_SIZE(ctx) - ARR_DATA_OFFSET(ctx) <= pos_idx * 4)
+        {
+            elog(ERROR, "Array out of bounds access: %ld %d", 
+                 ARR_SIZE(ctx) - ARR_DATA_OFFSET(ctx), pos_idx * 4);
+        }
+        ctx_data = (int32_t *) ARR_DATA_PTR(ctx);
         ctx_data[pos_idx]++;
     }
     
